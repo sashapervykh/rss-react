999adc60f806dc956f980198aa960063442859b9
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-07-10 18:58:41 +0300

    feat: add no_image file

--- rs-react-app/src/components/SearchResults/SearchResults.tsx
+++ rs-react-app/src/components/SearchResults/SearchResults.tsx
@@ -1,6 +1,7 @@
 import { Component, type ReactNode } from 'react';
 import type { SearchResultType } from '../../api/getDataFromApi';
-import { Card } from '../Card/card';
+import { Card } from '../Card/Card';
+import NO_IMAGE from '../../../public/no_image_available.png';
 
 interface Props {
   results?: SearchResultType[];
@@ -14,7 +15,7 @@ export class SearchResults extends Component<Props> {
           ? this.props.results.map((element, index) => (
               <Card
                 key={index}
-                img={element.image_url ?? ''}
+                img={element.image_url ?? NO_IMAGE}
                 title={element.title}
                 description={element.description}
               />
