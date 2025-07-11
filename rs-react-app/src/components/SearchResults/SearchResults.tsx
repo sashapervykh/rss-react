68cd92b64e2faf12ff868dc2d1a7fe1ad48126ba
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-07-11 11:26:33 +0300

    feat: add different statements for no result and for undefined results

--- rs-react-app/src/components/SearchResults/SearchResults.tsx
+++ rs-react-app/src/components/SearchResults/SearchResults.tsx
@@ -12,16 +12,18 @@ export class SearchResults extends Component<Props> {
     return (
       <div>
         {this.props.results
-          ? this.props.results.map((element, index) => (
-              <Card
-                key={index}
-                source={element.source ?? NO_IMAGE}
-                title={element.title}
-                media_type={element.media_type}
-                description={element.description}
-              />
-            ))
-          : 'No results'}
+          ? this.props.results.length === 0
+            ? 'Nothing was found on your request. Try to change input to get results (e.g. enter the whole word, not its part)'
+            : this.props.results.map((element, index) => (
+                <Card
+                  key={index}
+                  source={element.source ?? NO_IMAGE}
+                  title={element.title}
+                  media_type={element.media_type}
+                  description={element.description}
+                />
+              ))
+          : `Enter your word and press 'Search' to start a journey!`}
       </div>
     );
   }
