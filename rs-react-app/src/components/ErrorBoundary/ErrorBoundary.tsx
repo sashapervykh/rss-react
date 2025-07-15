6a582859da64685bd24211b6c25cbd046d15b827
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-07-15 15:17:32 +0300

    refactor: remove clearing local storage by button reset

--- rs-react-app/src/components/ErrorBoundary/ErrorBoundary.tsx
+++ rs-react-app/src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -1,6 +1,5 @@
 import { Component, type ReactNode } from 'react';
 import { ErrorFallback } from '../ErrorFallback/ErrorFallback';
-import { clearLocalStorage } from '../../utilities/clearLocalStorage';
 
 interface Props {
   children: ReactNode;
@@ -37,7 +36,6 @@ export class ErrorBoundary extends Component<Props, State> {
   }
 
   resetErrorState = () => {
-    clearLocalStorage();
     this.setState({ hasError: false, error: undefined });
   };
 }
