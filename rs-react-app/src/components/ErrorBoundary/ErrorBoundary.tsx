9f28abb777699af0ac20d78d96ea3afa4abd37ef
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-07-15 14:35:52 +0300

    fix: add ReactNode type

--- rs-react-app/src/components/ErrorBoundary/ErrorBoundary.tsx
+++ rs-react-app/src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { Component, type ReactNode } from 'react';
 import { ErrorFallback } from '../ErrorFallback/ErrorFallback';
 import { clearLocalStorage } from '../../utilities/clearLocalStorage';
 
