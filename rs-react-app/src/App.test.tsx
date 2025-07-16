3fa48763464aa46a19fd3b34ec8371bd1fb3f576
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-07-16 12:24:55 +0300

    fix: adjsut vitest settings for correct work

---
+++ rs-react-app/src/App.test.tsx
@@ -0,0 +1,15 @@
+import { it, expect, describe } from 'vitest';
+import { screen, render } from '@testing-library/react';
+import App from './App';
+
+describe('App tests', () => {
+  it('should render the title', () => {
+    render(<App />);
+
+    expect(
+      screen.getByRole('heading', {
+        level: 1,
+      })
+    ).toHaveTextContent('BROWSE');
+  });
+});
--- rs-react-app/vitest.setup.ts
+++ rs-react-app/vitest.setup.ts
@@ -1,6 +1,6 @@
-import '@testing-library/jest-dom';
 import { afterEach, expect } from 'vitest';
 import { cleanup } from '@testing-library/react';
+
 import * as matchers from '@testing-library/jest-dom/matchers';
 
 expect.extend(matchers);
