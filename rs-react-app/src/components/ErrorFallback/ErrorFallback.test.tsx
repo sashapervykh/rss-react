9173a66efd4c220cdc5674ede3c35a3e9eec3d20
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-07-18 14:58:14 +0300

    test: add test for ErrorFallback component

---
+++ rs-react-app/src/components/ErrorFallback/ErrorFallback.test.tsx
@@ -0,0 +1,31 @@
+import { render, screen } from '@testing-library/react';
+import { expectFallbackUI } from '../../test-utils/expectFallbackUI';
+import { ErrorBoundary } from '../ErrorBoundary/ErrorBoundary';
+import { ErrorFallback } from './ErrorFallback';
+import userEvent from '@testing-library/user-event';
+import { BreakingButton } from '../BreakingButton/BreakingButton';
+
+describe('ErrorFallback', () => {
+  it(`should render fallback UI`, () => {
+    render(<ErrorFallback message="Testing Fallback UI" reset={vi.fn} />);
+
+    expectFallbackUI('Testing Fallback UI');
+  });
+  it(`should reset state by pressing "Reset" button`, async () => {
+    render(
+      <ErrorBoundary>
+        <BreakingButton />
+      </ErrorBoundary>
+    );
+
+    const initialButton = screen.getByRole('button', { name: 'BREAK!' });
+
+    await userEvent.click(initialButton);
+    const resetButton = screen.getByRole('button', { name: 'Reset' });
+    await userEvent.click(resetButton);
+
+    const rerenderedButton = screen.getByRole('button', { name: 'BREAK!' });
+
+    expect(rerenderedButton).toBeInTheDocument();
+  });
+});
