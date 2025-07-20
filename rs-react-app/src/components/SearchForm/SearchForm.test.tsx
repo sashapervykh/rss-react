0959b516c0626a7f5f39b42543076c3dceb21151
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-07-20 16:19:44 +0300

    refactor: remove several repeats in SearchForm components

--- rs-react-app/src/components/SearchForm/SearchForm.test.tsx
+++ rs-react-app/src/components/SearchForm/SearchForm.test.tsx
@@ -6,16 +6,17 @@ describe('SearchForm', () => {
   const renderForm = (disabled: boolean = false) => {
     const fn = vi.fn();
     render(<SearchForm handleSearch={fn} disabled={disabled} />);
-    return fn;
+    return {
+      fn: fn,
+      input: screen.getByRole('textbox'),
+      buttons: screen.getAllByRole('button'),
+      searchButton: screen.getByRole('button', { name: 'Search' }),
+      breakButton: screen.getByRole('button', { name: 'BREAK!' }),
+    };
   };
 
   it(`should render disabled input and two buttons (search button is disabled, break button is active) when get disabled=true`, () => {
-    renderForm(true);
-
-    const input = screen.getByRole('textbox');
-    const buttons = screen.getAllByRole('button');
-    const searchButton = screen.getByRole('button', { name: 'Search' });
-    const breakButton = screen.getByRole('button', { name: 'BREAK!' });
+    const { input, buttons, searchButton, breakButton } = renderForm(true);
 
     expect(input).toBeInTheDocument();
     expect(input).toBeDisabled();
@@ -24,12 +25,7 @@ describe('SearchForm', () => {
     expect(breakButton).not.toBeDisabled();
   });
   it(`should render active input and two active buttons when get disabled=false`, () => {
-    renderForm(false);
-
-    const input = screen.getByRole('textbox');
-    const buttons = screen.getAllByRole('button');
-    const searchButton = screen.getByRole('button', { name: 'Search' });
-    const breakButton = screen.getByRole('button', { name: 'BREAK!' });
+    const { input, buttons, searchButton, breakButton } = renderForm(false);
 
     expect(input).toBeInTheDocument();
     expect(input).not.toBeDisabled();
@@ -38,33 +34,7 @@ describe('SearchForm', () => {
     expect(breakButton).not.toBeDisabled();
   });
   it(`should update input value when user typing`, async () => {
-    renderForm(false);
-
-    const input = screen.getByRole('textbox');
-
-    if (!(input instanceof HTMLInputElement))
-      throw new Error('The element is not an input');
-
-    await userEvent.type(input, 'input');
-
-    expect(input).toHaveValue('input');
-  });
-  it(`should update input value when user typing`, async () => {
-    renderForm(false);
-
-    const input = screen.getByRole('textbox');
-
-    if (!(input instanceof HTMLInputElement))
-      throw new Error('The element is not an input');
-
-    await userEvent.type(input, 'input');
-
-    expect(input).toHaveValue('input');
-  });
-  it(`should update input value when user typing`, async () => {
-    renderForm(false);
-
-    const input = screen.getByRole('textbox');
+    const { input } = renderForm(false);
 
     if (!(input instanceof HTMLInputElement))
       throw new Error('The element is not an input');
@@ -74,9 +44,7 @@ describe('SearchForm', () => {
     expect(input).toHaveValue('input');
   });
   it(`should save entered trimmed search term in localStorage when pressing submit button`, async () => {
-    renderForm(false);
-
-    const input = screen.getByRole('textbox');
+    const { input } = renderForm(false);
 
     if (!(input instanceof HTMLInputElement))
       throw new Error('The element is not an input');
@@ -90,9 +58,7 @@ describe('SearchForm', () => {
   it(`should used term saved in localStorage`, async () => {
     localStorage.setItem('input', 'input');
 
-    renderForm(false);
-
-    const input = screen.getByRole('textbox');
+    const { input } = renderForm(false);
 
     if (!(input instanceof HTMLInputElement))
       throw new Error('The element is not an input');
@@ -100,7 +66,7 @@ describe('SearchForm', () => {
     expect(input).toHaveValue('input');
   });
   it(`should call handleSearch function when click on 'Search' button`, async () => {
-    const fn = renderForm(false);
+    const { fn } = renderForm(false);
 
     const button = screen.getByRole('button', { name: 'Search' });
     await userEvent.click(button);
