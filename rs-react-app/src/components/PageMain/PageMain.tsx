b3f8b22305c29e188c8911ce5f3c8115efbbe1a4
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-07-22 13:31:57 +0300

    refactor: turn PageMain and PageTitle into function components

--- rs-react-app/src/components/PageMain/PageMain.tsx
+++ rs-react-app/src/components/PageMain/PageMain.tsx
@@ -1,14 +1,11 @@
-import { Component, type ReactNode } from 'react';
 import { PageTitle } from '../PageTitle/PageTitle';
 import { SearchWrapper } from '../SearchWrapper/SearchWrapper';
 
-export class PageMain extends Component {
-  render(): ReactNode {
-    return (
-      <main>
-        <PageTitle />
-        <SearchWrapper />
-      </main>
-    );
-  }
+export function PageMain() {
+  return (
+    <main>
+      <PageTitle />
+      <SearchWrapper />
+    </main>
+  );
 }
