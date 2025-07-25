1d91912313171b11317cd0519d1031178222fdcd
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-07-25 13:11:44 +0300

    feat: add react-router

--- rs-react-app/src/App.tsx
+++ rs-react-app/src/App.tsx
@@ -1,8 +1,13 @@
+import { Route, Routes } from 'react-router';
 import './App.css';
 import { PageMain } from './components/PageMain/PageMain';
 
 function App() {
-  return <PageMain />;
+  return (
+    <Routes>
+      <Route path="/" element={<PageMain />}></Route>
+    </Routes>
+  );
 }
 
 export default App;
