83caa3095557a1ae259fe20e998f570280c893e1
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-07-27 18:07:57 +0300

    test: test layout

---
+++ rs-react-app/src/components/Layout/Layout.test.tsx
@@ -0,0 +1,30 @@
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router';
+import { Layout } from './Layout';
+
+describe('Layout', () => {
+  it(`should render about link`, async () => {
+    render(
+      <MemoryRouter initialEntries={['/home']}>
+        <Layout />
+      </MemoryRouter>
+    );
+
+    const link = await screen.findByRole('link');
+
+    expect(link).toBeInTheDocument();
+    expect(link).toHaveTextContent('About');
+  });
+  it(`should render home link`, async () => {
+    render(
+      <MemoryRouter initialEntries={['/about']}>
+        <Layout />
+      </MemoryRouter>
+    );
+
+    const link = await screen.findByRole('link');
+
+    expect(link).toBeInTheDocument();
+    expect(link).toHaveTextContent('Home');
+  });
+});
