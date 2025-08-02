96817e608d3020e31d185f06a2630187b991d162
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-08-02 14:15:16 +0300

    feat: save file by button click

--- rs-react-app/src/components/Flyout/Flyout.tsx
+++ rs-react-app/src/components/Flyout/Flyout.tsx
@@ -2,17 +2,49 @@ import { useCustomDispatch, useCustomSelector } from '../../hooks/reduxHooks';
 import { cardSlice } from '../../store/reducers/CardSlice';
 import { Button } from '../Button/Button';
 import style from './style.module.css';
+import { useRef, useState } from 'react';
 
 export function Flyout() {
   const { amount } = useCustomSelector((state) => state.CardReducer);
   const { clear } = cardSlice.actions;
   const dispatch = useCustomDispatch();
+  const [downloadUrl, setDownloadUrl] = useState('');
+  const downloadRef = useRef<HTMLAnchorElement>(null);
+  const [fileName, setFileName] = useState('');
+
+  const handleDownloadClick = () => {
+    const fileContent = 'file content';
+    const blob = new Blob([fileContent], { type: 'text/csv' });
+    const url = URL.createObjectURL(blob);
+
+    setDownloadUrl(url);
+    setFileName(`${amount}_item${amount === 1 ? '' : 's'}.csv`);
+
+    setTimeout(() => {
+      if (downloadRef.current) {
+        downloadRef.current.click();
+        URL.revokeObjectURL(url);
+        setDownloadUrl('');
+      }
+    }, 0);
+  };
 
   return (
     amount && (
       <div className={style.flyout}>
         <div>Selected items: {amount}</div>
         <Button text="Unselect all" onClick={() => dispatch(clear())} />
+        <Button
+          text="Download"
+          disabled={amount === 0 ? true : false}
+          onClick={handleDownloadClick}
+        />
+        <a
+          ref={downloadRef}
+          href={downloadUrl}
+          className={style.link}
+          download={fileName}
+        />
       </div>
     )
   );
