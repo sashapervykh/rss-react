5cc432f9530f0c274ad9ec3fbb6df71380a60e19
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-08-03 17:39:16 +0300

    test: test createFileContent

---
+++ rs-react-app/src/utilities/createFileContent.test.ts
@@ -0,0 +1,23 @@
+import {
+  mockedRequestResultWithoutDescription,
+  mockedSimpleRequestResult,
+} from '../test-utils/mockedCardsData';
+import { createFileContent } from './createFileContent';
+
+describe('createFileContent', () => {
+  it(`should return correct string for one card`, async () => {
+    expect(createFileContent([mockedSimpleRequestResult.results[0]])).toEqual(
+      '"simple","Testing data for simple","image","test.jpg","simple"'
+    );
+  });
+  it(`should return correct string for several cards`, async () => {
+    expect(
+      createFileContent([
+        mockedSimpleRequestResult.results[0],
+        mockedRequestResultWithoutDescription.results[0],
+      ])
+    ).toEqual(
+      '"simple","Testing data for simple","image","test.jpg","simple"\n"without description","NASA did not provide any description for this item(((","video","test.jpg","without description"'
+    );
+  });
+});
