e9a849da594a91b7ebc433d4e3938f7951fb293f
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-08-03 14:00:16 +0300

    fix: fix long text displaying

--- rs-react-app/src/api/getDataFromApi.ts
+++ rs-react-app/src/api/getDataFromApi.ts
@@ -26,19 +26,9 @@ export async function getDataFromApi({ input = '', page = 1 }: Props) {
 
     const maxPage = Math.ceil(typedBody.collection.metadata.total_hits / 10);
     const searchResult = typedBody.collection.items.map((element) => {
-      const title =
-        element.data[0].title.length > 30
-          ? element.data[0].title.substring(0, 30) + '...'
-          : element.data[0].title;
-
-      const description =
-        element.data[0].description && element.data[0].description.length > 140
-          ? element.data[0].description.substring(0, 140) + '...'
-          : element.data[0].description;
       return {
-        title: title,
+        title: element.data[0].title,
         description:
-          description ??
           element.data[0].description ??
           `NASA did not provide any description for this item(((`,
         media_type: element.data[0].media_type,
