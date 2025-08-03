0ddb8bbf1823923e6dbffa35eea8f89a0f5eb960
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-08-03 18:46:42 +0300

    test: test theme button

---
+++ rs-react-app/src/components/Header/Header.test.tsx
@@ -0,0 +1,37 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../../App';
+import { ThemeProvider } from '../../hooks/useTheme/ThemeProvider';
+import { MemoryRouter } from 'react-router';
+
+describe('Header', () => {
+  const renderPage = () => {
+    render(
+      <ThemeProvider>
+        <MemoryRouter>
+          <App />
+        </MemoryRouter>
+      </ThemeProvider>
+    );
+  };
+
+  it(`should change theme by click on theme button`, async () => {
+    renderPage();
+
+    const button = screen.getByRole('button', { name: 'Dark' });
+    await userEvent.click(button);
+
+    expect(document.documentElement).toHaveClass('background-light');
+    expect(button).toHaveTextContent('Light');
+  });
+  it(`should change theme to initial by double click`, async () => {
+    renderPage();
+
+    const button = screen.getByRole('button', { name: 'Dark' });
+    await userEvent.click(button);
+    await userEvent.click(button);
+
+    expect(document.documentElement).toHaveClass('background-dark');
+    expect(button).toHaveTextContent('Dark');
+  });
+});
