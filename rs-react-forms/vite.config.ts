bb0f56d315cabc85e314ba5fcab1fee5ff3578f2
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-08-24 14:47:29 +0300

    feat: add coverage config

--- rs-react-forms/vite.config.ts
+++ rs-react-forms/vite.config.ts
@@ -8,5 +8,19 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./src/setupTests.ts'],
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+      include: ['src/**/*.{js,jsx,ts,tsx}'],
+      exclude: [
+        'src/test-utils/*',
+        'src/**/*.test.{js,jsx,ts,tsx}',
+        'src/**/*.spec.{js,jsx,ts,tsx}',
+        'src/index.{js,jsx,ts,tsx}',
+        'src/setupTests.{js,ts}',
+        'src/**/*.d.ts',
+      ],
+
+      thresholds: { statements: 80, branches: 50, functions: 50, lines: 50 },
+    },
   },
 });
