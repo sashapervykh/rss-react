f210a86b51c07b5f02a633a28b1fd3cfbc5a18f2
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-08-24 11:48:25 +0300

    feat: close modal by Esc and by outside click

--- rs-react-forms/src/components/Modal/Modal.tsx
+++ rs-react-forms/src/components/Modal/Modal.tsx
@@ -3,14 +3,38 @@ import style from './style.module.css';
 import { UncontrolledForm } from '../UncontrolledForm/UncontrolledForm';
 import { useModal } from '../../hooks/useModal/useModal';
 import { RHForm } from '../RHForm/RHForm';
+import { useEffect } from 'react';
 
 export function Modal() {
-  const { modal } = useModal();
+  const { modal, toggleModal } = useModal();
+
+  useEffect(() => {
+    function handleEscPress(event: KeyboardEvent) {
+      if (event.key === 'Escape') toggleModal(null);
+    }
+
+    document.addEventListener('keydown', handleEscPress);
+
+    return () => {
+      document.removeEventListener('keydown', handleEscPress);
+    };
+  }, [toggleModal]);
 
   if (!modal) return;
 
   return createPortal(
-    <dialog className={style['modal-wrapper']}>
+    <dialog
+      className={style['modal-wrapper']}
+      role="dialog"
+      aria-modal="true"
+      onClick={(event) => {
+        const clickedElement = event.target;
+        if (clickedElement instanceof HTMLElement) {
+          const form = clickedElement.closest('form');
+          if (!form) toggleModal(null);
+        }
+      }}
+    >
       {modal === 'uncontrolled' && <UncontrolledForm />}
       {modal === 'rhf' && <RHForm />}
     </dialog>,
