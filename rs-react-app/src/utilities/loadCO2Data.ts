ccccef197f23015d3348d7e106320bae9af1561e
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-08-28 16:53:09 +0300

    refactor: change type of data given to table

--- rs-react-app/src/utilities/loadCO2Data.ts
+++ rs-react-app/src/utilities/loadCO2Data.ts
@@ -1,16 +1,22 @@
-import type { z } from 'zod/v4';
-import { DataSchema } from '../models/schema';
+import { DatasetSchema, type DisplayedDataType } from '../models/schema';
 
-let dataPromise: Promise<z.infer<typeof DataSchema>>;
+let dataPromise: Promise<DisplayedDataType>;
 
 export function loadCO2Data() {
   if (!dataPromise) {
     dataPromise = fetch('/co2-data.json')
       .then((res) => res.json())
       .then((json) => {
-        console.log(json);
-        const typedData = DataSchema.parse(json);
-        return typedData;
+        const dataArray: DisplayedDataType = [];
+        const typedData = DatasetSchema.parse(json);
+        Object.entries(typedData).map((elem) => {
+          dataArray.push({
+            name: elem[0],
+            iso_code: elem[1].iso_code,
+            data: elem[1].data,
+          });
+        });
+        return dataArray;
       })
       .catch(() => {
         throw new Error('Data with the wrong type was received!');
